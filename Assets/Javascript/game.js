1aa54fbb56941b21db0f144614f4d5c89878b037
ondelhylton <46361132+ondelhylton@users.noreply.github.com>
2019-02-05 18:54:15 -0500

    Add files via upload

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,74 +1,58 @@
-<!DOCTYPE html>
-<html lang="en-us">
 
-<head>
-  <meta charset="UTF-8">
-  <title>Rock Paper Scissors Part 7</title>
-</head>
+var computerpicks = ["a", "e", "i", "o", "u",];
+ 
 
-<body>
+var wins = 0;
+var losses = 0;
+var guesses = 10;
 
-  <script type="text/javascript">
+    var userlog = document.getElementById("log");
+    var guessestext = document.getElementById("guesses-text");
+    var winstext = document.getElementById("wins-text");
+    var lossestext = document.getElementById("losses-text");
 
-    var computerpicks = ["a", "e", "i", "o", "u",];
-     
-    
-    var wins = 0;
-    var losses = 0;
-    var guesses = 10;
-    
-        var userlog = document.getElementById("log");
-        var guessestext = document.getElementById("guesses-text");
-        var winstext = document.getElementById("wins-text");
-        var lossestext = document.getElementById("losses-text");
-    
-    
-    
-    document.onkeyup = function(event) {
-    var userguesses = event.key;
-    
-    
-    
-    var computerguess = computerpicks[Math.floor(Math.random() * computerpicks.length)];
-    
-    
-    
-        if ( userguesses === computerguess) {
-            wins++; 
-    
-        } else {
-            losses++;
-        }
-    
-        if ( userguesses === computerguess) {
-            guesses--; 
-    
-        }
-    
-        if ( userguesses != computerguess) {
-            guesses--; 
-    
-        }
-    
-        if ((userguesses != "a") && (userguesses != "e") && (userguesses != "i") && (userguesses != "o") && (userguesses != "u")) {
-    
-        alert("That's not a vowel!");
-        }
-    
-        
-    
-            winstext.textContent = "Wins: " + wins;
-            lossestext.textContent = "Losses: " + losses;
-            guessestext.textContent = "Guesses left: " + guesses;
-            userlog.innerHTML= "Letters Guessed: " + userguesses;   
-    
-            
+
+
+document.onkeyup = function(event) {
+var userguesses = event.key;
+
+
+
+var computerguess = computerpicks[Math.floor(Math.random() * computerpicks.length)];
+
+
+
+    if ( userguesses === computerguess) {
+        wins++; 
+
+    } else {
+        losses++;
     }
-     // Need to run function in a loop nine times, declare winner and start over. 
-     //Separate game using all letters and letting a user have nine tries
+
+    if ( userguesses === computerguess) {
+        guesses--; 
+
+    }
+
+    if ( userguesses != computerguess) {
+        guesses--; 
+
+    }
+
+    if ((userguesses != "a") && (userguesses != "e") && (userguesses != "i") && (userguesses != "o") && (userguesses != "u")) {
+
+    alert("That's not a vowel!");
+    }
+
     
-    </script>
 
-</body>
+        winstext.textContent = "Wins: " + wins;
+        lossestext.textContent = "Losses: " + losses;
+        guessestext.textContent = "Guesses left: " + guesses;
+        userlog.innerHTML= "Letters Guessed: " + userguesses[i];   
 
-</html>
+        
+}
+ // Need to run function in a loop nine times, declare winner and start over. 
+ //Separate game using all letters and letting a user have nine tries
+
